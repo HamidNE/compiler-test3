b53bc1955fc9027bca81311561ce43d2e5c05589
Hamid Nasr <hamidne@mail.ru>
2018-06-23 23:05:21 +0430

    update yacc and lex file and compile them

--- y.tab.c
+++ y.tab.c
@@ -100,12 +100,14 @@ Maintained by Magnus Ekdahl <magnus@debian.org>
  #line 88 "/usr/share/bison++/bison.cc"
 #line 1 "compiler.y"
 
-    #include <stdio.h>
-    #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-    extern int yylex();
+extern int yylex();
+void yyerror(char *msg);
 
-#line 8 "compiler.y"
+
+#line 10 "compiler.y"
 typedef union {
     float f;
 } yy_parse_stype;
@@ -473,7 +475,7 @@ static const short yyrhs[] = {    11,
 
 #if (YY_parse_DEBUG != 0) || defined(YY_parse_ERROR_VERBOSE) 
 static const short yyrline[] = { 0,
-    17,    20,    21,    22,    25,    26,    27,    30,    31,    32
+    19,    22,    23,    24,    27,    28,    29,    32,    33,    34
 };
 
 static const char * const yytname[] = {   "$","error","$illegal.","NUM","'+'",
@@ -1015,35 +1017,35 @@ YYLABEL(yyreduce)
   switch (yyn) {
 
 case 1:
-#line 17 "compiler.y"
+#line 19 "compiler.y"
 { printf("%f\n", yyvsp[0].f); ;
     break;}
 case 2:
-#line 20 "compiler.y"
+#line 22 "compiler.y"
 { yyval.f = yyvsp[-2].f + yyvsp[0].f; ;
     break;}
 case 3:
-#line 21 "compiler.y"
+#line 23 "compiler.y"
 { yyval.f = yyvsp[-2].f - yyvsp[0].f; ;
     break;}
 case 5:
-#line 25 "compiler.y"
+#line 27 "compiler.y"
 { yyval.f = yyvsp[-2].f * yyvsp[0].f; ;
     break;}
 case 6:
-#line 26 "compiler.y"
+#line 28 "compiler.y"
 { yyval.f = yyvsp[-2].f / yyvsp[0].f; ;
     break;}
 case 8:
-#line 30 "compiler.y"
+#line 32 "compiler.y"
 { yyval.f = yyvsp[-1].f; ;
     break;}
 case 9:
-#line 31 "compiler.y"
+#line 33 "compiler.y"
 { yyval.f = -yyvsp[0].f; ;
     break;}
 case 10:
-#line 32 "compiler.y"
+#line 34 "compiler.y"
 { yyval.f = yyvsp[0].f; ;
     break;}
 }
@@ -1250,7 +1252,7 @@ YYLABEL(yyerrhandle)
 /* END */
 
  #line 1038 "/usr/share/bison++/bison.cc"
-#line 35 "compiler.y"
+#line 37 "compiler.y"
 
 
 void yyerror(char *msg) {
--- y.tab.h
+++ y.tab.h
@@ -13,7 +13,7 @@
 
  #line 8 "/usr/share/bison++/bison.h"
 
-#line 8 "compiler.y"
+#line 10 "compiler.y"
 typedef union {
     float f;
 } yy_parse_stype;
